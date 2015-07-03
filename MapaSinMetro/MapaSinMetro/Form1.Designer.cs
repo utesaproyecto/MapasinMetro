f529b19aa6e5b796f534c4c507f8eb1f317db0ad
UtesaProyecto <utesaproyecto1@gmail.com>
2015-07-03 00:57:41 -0400

    nuevo
    nuevoqq

---
+++ MapaSinMetro/MapaSinMetro/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace MapaSinMetro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbxCalle = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.txtbxCiudad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbxMunicipio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbxPais = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtbxPais);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.txtbxMunicipio);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.txtbxCiudad);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.txtbxCalle);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(916, 432);
+            this.splitContainer1.SplitterDistance = 240;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "calle";
+            // 
+            // txtbxCalle
+            // 
+            this.txtbxCalle.Location = new System.Drawing.Point(15, 50);
+            this.txtbxCalle.Name = "txtbxCalle";
+            this.txtbxCalle.Size = new System.Drawing.Size(152, 22);
+            this.txtbxCalle.TabIndex = 2;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(672, 432);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // txtbxCiudad
+            // 
+            this.txtbxCiudad.Location = new System.Drawing.Point(15, 97);
+            this.txtbxCiudad.Name = "txtbxCiudad";
+            this.txtbxCiudad.Size = new System.Drawing.Size(152, 22);
+            this.txtbxCiudad.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ciudad";
+            // 
+            // txtbxMunicipio
+            // 
+            this.txtbxMunicipio.Location = new System.Drawing.Point(15, 148);
+            this.txtbxMunicipio.Name = "txtbxMunicipio";
+            this.txtbxMunicipio.Size = new System.Drawing.Size(152, 22);
+            this.txtbxMunicipio.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "municipio";
+            // 
+            // txtbxPais
+            // 
+            this.txtbxPais.Location = new System.Drawing.Point(15, 202);
+            this.txtbxPais.Name = "txtbxPais";
+            this.txtbxPais.Size = new System.Drawing.Size(152, 22);
+            this.txtbxPais.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "pais";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 432);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtbxPais;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtbxMunicipio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbxCiudad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbxCalle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ MapaSinMetro/MapaSinMetro/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapaSinMetro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StringBuilder queryinicio = new StringBuilder();
+            queryinicio.Append("https://www.google.com.do/maps/place/Rep%C3%BAblica+Dominicana/@18.8232809,-71.1661454,7z/data=!4m2!3m1!1s0x8eaf8838def1b6f5:0xa6020f24060df7e0?hl=es");
+
+            webBrowser1.Navigate(queryinicio.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string calle = txtbxCalle.Text;
+            string ciudad = txtbxCiudad.Text;
+            string municipio = txtbxMunicipio.Text;
+            string pais = txtbxPais.Text;
+            try
+            {
+                StringBuilder querydireccion = new StringBuilder();
+                querydireccion.Append("http://maps.google.com/maps?q=");
+
+                if (calle != string.Empty)
+                {
+                    querydireccion.Append(calle + "," + "+");
+                }
+                if (ciudad != string.Empty)
+                {
+                    querydireccion.Append(ciudad + "," + "+");
+                }
+                if (municipio != string.Empty)
+                {
+                    querydireccion.Append(municipio + "," + "+");
+                }
+                if (pais != string.Empty)
+                {
+                    querydireccion.Append(pais + "," + "+");
+                }
+
+                webBrowser1.Navigate(querydireccion.ToString());
+
+            }//final try
+            catch (Exception ex)
+            {
+                //MetroMessageBox.Show(this, ex.Message.ToString(), "Error", 220);
+                MessageBox.Show(ex.Message.ToString() );
+            }
+        }
+    
+    
+    
+    
+    }
+}
